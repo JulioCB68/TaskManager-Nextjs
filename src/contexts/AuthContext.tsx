2277661a58e2f5966fffaaeec198e3503674332d
Júlio César <juliobento237@gmail.com>
2023-12-07 20:32:32 -0300

    (feat): creating Authcontext with contextAPI

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,70 @@
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { parseCookies, destroyCookie, setCookie } from 'nookies'
+
+import { getUserGoogle } from "@/services/getUserGoogle";
+import { GithubUserData, GoogleUserProfile } from "@/services/types";
+import { getGithubToken } from "@/services/githubToken";
+
+interface IAuthContext {
+  user: User | null;
+  isAuthenticated: boolean;
+  loginWithGoogle: (accessToken: string) => void;
+  loginWithGithub: (accessToken: string) => void;
+  logout: () => void
+}
+
+export const AuthContext = createContext({} as IAuthContext)
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const cookies = parseCookies();
+
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() =>
+    !!cookies.access_token_google || !!cookies.access_token_github
+	);
+
+  useEffect(() => {
+    isAuthenticated
+  }, [isAuthenticated]);
+
+  async function loginWithGoogle(accessToken: string) {
+    try {
+      const userData = await getUserGoogle(accessToken);
+      setUser(userData)
+    } catch (error) {
+      console.error('loginWithGoogle Error fetching user data:', error);
+    }
+  }
+
+  async function loginWithGithub(code: string) {
+    try {
+      const userData = await getGithubToken(code);
+      setUser(userData);
+    } catch (error) {
+      console.error('loginWithGithub Error fetching user data:', error);
+      throw error
+    }
+  }
+
+  function logout() {
+    Object.keys(cookies).forEach(cookieName => {
+      destroyCookie(null, cookieName);
+    });
+    
+    setUser(null)
+	}
+
+  const contextValue: IAuthContext = {
+    user,
+    isAuthenticated,
+    loginWithGoogle,
+    loginWithGithub,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
