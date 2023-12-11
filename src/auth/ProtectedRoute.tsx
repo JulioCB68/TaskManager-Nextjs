411fb37e56495b5784da50bd9b7db8f952bbd5f9
Júlio César <juliobento237@gmail.com>
2023-12-11 17:00:49 -0300

    (fix): fixing authentication feature

---
+++ src/auth/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { GetServerSidePropsContext } from 'next'
+import nookies from 'nookies'
+
+export async function ProtectedRoute(context: GetServerSidePropsContext) {
+  const cookies = nookies.get(context)
+
+  const hasToken = cookies.access_token_google
+
+  if (!hasToken) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+  return { props: {} }
+}
