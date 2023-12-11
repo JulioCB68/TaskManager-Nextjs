56f1c2a29f4b94198bb00115b9a40ef59eb1e63c
Júlio César <juliobento237@gmail.com>
2023-12-11 16:56:38 -0300

    (feat): creating task component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -8,7 +8,7 @@ interface ITaskProps {
 
 export function Task({ task }: ITaskProps) {
   return (
-    <div className="py-12">
+    <div className="py-2">
       <div className="cursor-pointer rounded-md bg-white p-4">
         <div className="flex items-center justify-between">
           <p className="font-bold">{task.name}</p>
@@ -24,14 +24,14 @@ export function Task({ task }: ITaskProps) {
               className={`${
                 task.status === 'done'
                   ? 'cursor-pointer pl-1 font-semibold text-gray-400'
-                  : 'text-shadowBlue cursor-pointer pl-1 font-semibold'
+                  : 'cursor-pointer pl-1 font-semibold text-shadowBlue'
               }`}
             />
             <p
               className={`${
                 task.status === 'done'
                   ? 'pl-1 font-semibold text-gray-400'
-                  : 'text-shadowBlue pl-1 font-semibold'
+                  : 'pl-1 font-semibold text-shadowBlue'
               }`}
             >
               {task.comments}
