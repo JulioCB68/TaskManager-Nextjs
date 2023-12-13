4f465c1711252935149c22bb187be289c2c89655
Júlio César <juliobento237@gmail.com>
2023-12-13 17:28:47 -0300

    (fix): fixing header layoult

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,6 @@ import { parseCookies } from 'nookies'
 
 import { useAuth } from '@/contexts/AuthContext'
 
-import { Button } from './Button'
-
 export function Header() {
   const router = useRouter()
   const cookies = parseCookies()
@@ -21,24 +19,14 @@ export function Header() {
       <h1 className="text-xl font-semibold">Task Manager Dev</h1>
       <div className="flex items-center space-x-4">
         {user && (
-          <div className="flex items-center space-x-4">
-            <div className="relative h-12 w-12 overflow-hidden rounded-full">
-              <Image
-                className="cursor-pointer"
-                src={user?.avatar}
-                alt="User Avatar"
-                objectFit="cover"
-                width={50}
-                height={50}
-              />
-            </div>
-            <span className="hidden md:inline-block">{user.name}</span>
-            <Button
-              text="Logout"
-              className="flex items-center justify-center rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600"
-              onClick={handleLogout}
-            />
-          </div>
+          <Image
+            className="cursor-pointer rounded-full"
+            src={user?.avatar ?? ''}
+            alt="User Avatar"
+            objectFit="cover"
+            width={40}
+            height={40}
+          />
         )}
       </div>
     </header>
